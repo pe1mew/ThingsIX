6ec673b29cc2a3438429b270f5dc0f93ff4e1ab8
Remko welling <pe1mew@gmail.com>
2022-12-11 17:22:26 +0100

    Added chirpstack payload decoders

---
+++ ChirpstackDecoder/lht65_decoder.js
@@ -0,0 +1,30 @@
+// Decode uplink function.
+//
+// Input is an object with the following fields:
+// - bytes = Byte array containing the uplink payload, e.g. [255, 230, 255, 0]
+// - fPort = Uplink fPort.
+// - variables = Object containing the configured device variables.
+//
+// Output must be an object with the following fields:
+// - data = Object representing the decoded payload.
+function decodeUplink(input) {
+  return {
+    data: {
+      temp: 22.5
+    }
+  };
+}
+
+// Encode downlink function.
+//
+// Input is an object with the following fields:
+// - data = Object representing the payload that must be encoded.
+// - variables = Object containing the configured device variables.
+//
+// Output must be an object with the following fields:
+// - bytes = Byte array containing the downlink payload.
+function encodeDownlink(input) {
+  return {
+    bytes: [225, 230, 255, 0]
+  };
+}
